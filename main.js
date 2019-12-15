12541f7db7051668dd472bd2c4703ddb28e6d447
Margo <margo.bozhenova@gmail.com>
2019-12-16 01:11:41 +0300

    Add a method that logs the % of DNA in common

--- main.js
+++ main.js
@@ -1,52 +1,56 @@
-'use strict';
+"use strict";
 
-(function () {
+(function() {
+  // Returns a random DNA base
+  const returnRandBase = () => {
+    const dnaBases = ["A", "T", "C", "G"];
+    return dnaBases[Math.floor(Math.random() * 4)];
+  };
 
-// Returns a random DNA base
-const returnRandBase = () => {
-  const dnaBases = ['A', 'T', 'C', 'G'];
-  return dnaBases[Math.floor(Math.random() * 4)];
-};
-
-// Returns a random single stand of DNA containing 15 bases
-const mockUpStrand = () => {
-  const newStrand = [];
-  for (let i = 0; i < 15; i++) {
-    newStrand.push(returnRandBase());
-  }
-  return newStrand;
-};
-
-const pAequorFactory = (num, array) => {
-  return {
-    specimenNum: num,
-    dna: array,
-    mutate() {
-      const randIndex = Math.floor(Math.random() * 15);
-      let diffBase = returnRandBase(); 
-      while (this.dna[randIndex] === diffBase) {
-        diffBase = returnRandBase();
-        continue;
-      } 
-      this.dna[randIndex] = diffBase;
-      return this.dna;
+  // Returns a random strand of DNA containing 15 bases
+  const mockUpStrand = () => {
+    const newStrand = [];
+    for (let i = 0; i < 15; i++) {
+      newStrand.push(returnRandBase());
     }
-  }
-}
+    return newStrand;
+  };
 
-const pAequor = pAequorFactory(1, mockUpStrand());
-console.log(pAequor);
-console.log(pAequor.mutate());
+  const pAequorFactory = (num, array) => {
+    return {
+      specimenNum: num,
+      dna: array,
+      mutate() {
+        const randIndex = Math.floor(Math.random() * 15);
+        let diffBase = returnRandBase();
+        while (this.dna[randIndex] === diffBase) {
+          diffBase = returnRandBase();
+          continue;
+        }
+        this.dna[randIndex] = diffBase;
+        return this.dna;
+      },
+      compareDNA(object) {
+        let counter = 0;
+        for (let i = 0; i < this.dna.length; i++) {
+          if (this.dna[i] === object.dna[i]) {
+            counter++;
+          }
+        }
+        console.log(
+          `Specimen ${this.specimenNum} and specimen ${
+            object.specimenNum
+          } have ${(
+            (counter / this.dna.length) *
+            100
+          ).toFixed()}% DNA in common.`
+        );
+      },
+      willLikelySurvive() {}
+    };
+  };
 
-
-
-
-
-}());
-
-
-
-
-
-
-
+  const pAequor = pAequorFactory(1, mockUpStrand());
+  const pAequor2 = pAequorFactory(2, mockUpStrand());
+  pAequor.compareDNA(pAequor2);
+})();
