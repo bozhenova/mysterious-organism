837f2f9d9d75b47f5b8c7755ab92afed582ae772
Margo <margo.bozhenova@gmail.com>
2019-12-16 00:28:16 +0300

    Add factory function with a 'mutate' method

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 (function () {
-  
+
 // Returns a random DNA base
 const returnRandBase = () => {
   const dnaBases = ['A', 'T', 'C', 'G'];
@@ -16,6 +16,27 @@ const mockUpStrand = () => {
   }
   return newStrand;
 };
+
+const pAequorFactory = (num, array) => {
+  return {
+    specimenNum: num,
+    dna: array,
+    mutate() {
+      const randIndex = Math.floor(Math.random() * 15);
+      let diffBase = returnRandBase(); 
+      while (this.dna[randIndex] === diffBase) {
+        diffBase = returnRandBase();
+        continue;
+      } 
+      this.dna[randIndex] = diffBase;
+      return this.dna;
+    }
+  }
+}
+
+const pAequor = pAequorFactory(1, mockUpStrand());
+console.log(pAequor);
+console.log(pAequor.mutate());
 
 
 
